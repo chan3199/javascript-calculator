e8758f41eedca0215b8e8297029d6652c453a376
Kang Ye Ram <kyj5730@gmail.com>
2025-05-11 20:29:22 +0900

    feat: 예외처리 및 리팩토링

--- src/script.js
+++ src/script.js
@@ -1,111 +1,156 @@
-let userInput = "";
 
-function updateDisplay() {
-  document.getElementById("expression").innerText = userInput;
-}
+//let userExpression = "";
 
-function getUserInput(input) {
-  userInput += input;
-  updateDisplay();
-}
+// function initializeUserExpression() {
+//   userExpression = "";
+// }
 
-function eraseNumber() {
-  userInput = userInput.slice(0, -1);
-  updateDisplay();
-}
+// function updateExpressionDisplay() {
+//   document.getElementById("expression").innerText = userExpression;
+// }
 
-function allClear() {
-  userInput = "";
-  updateDisplay();
-  document.getElementById("result").innerText = "0";
-}
+// function getuserExpression(input) {
+//   const lastCharacter = userExpression.slice(-1);
 
-// oper1의 우선순위가 oper2보다 높으면 true 반환
-function operatorPriority(operator1, operator2) {
-  const priority = { "+": 1, "-": 1, "*": 2, "/": 2 };
-  return priority[operator1] > priority[operator2];
-}
+//   if (isOperator(lastCharacter) && isOperator(input)) {
+//     userExpression = userExpression.slice(0,-1) + input
+//   } else if (userExpression == "" && isOperator(input)) {
+//     return 
+//   } else {
+//     userExpression += input;
+//   }
+//   updateExpressionDisplay();
+// }
 
-function getResult() {
-  let resultArray = userInput
-    .replace(/×/g, "*")
-    .replace(/÷/g, "/")
-    .split(/([+\-*/])/);
+// function isOperator(input) {
+//   const operators = ["+", "-", "×", "÷"];
 
-  // return getBackwardLotation(resultArray);
+//   return operators.includes(input)
+// }
 
-  document.getElementById("result").innerText =
-    getBackwardLotation(resultArray);
-}
+// function removeLastCharacter() {
+//   userExpression = userExpression.slice(0, -1);
+//   updateExpressionDisplay();
+// }
 
-function getBackwardLotation(resultArray) {
-  let operator = []; // 연산자
-  let result = [];
+// function clearAll() {
+//   initializeUserExpression()
+//   updateExpressionDisplay();
+//   document.getElementById("result").innerText = "0";
+// }
 
-  resultArray.forEach((element) => {
-    if (!isNaN(element)) {
-      result.push(element);
-    } else {
-      while (
-        operator.length > 0 &&
-        !operatorPriority(element, operator[operator.length - 1])
-      ) {
-        result.push(operator.pop());
-      }
-      operator.push(element);
-    }
-  });
+// function calculateResult() {
+//   const lastCharacter = userExpression.slice(-1)
+//   if (isOperator(lastCharacter)) {
+//     return alert("수식이 완성되지 않았습니다.");
+//   }
 
-  while (operator.length > 0) {
-    result.push(operator.pop());
-  }
+//   if (userExpression === "") {
+//     return document.getElementById("result").innerText = "0"
+//   }
 
-  return backwardCaculator(result);
-}
+//   let expressionArray = userExpression
+//     .replace(/×/g, "*")
+//     .replace(/÷/g, "/")
+//     .split(/([+\-*/])/);
 
-function calculator(operand1, operand2, operator) {
-  switch (operator) {
-    case "+":
-      return operand1 + operand2;
-    case "-":
-      return operand1 - operand2;
-    case "*":
-      return operand1 * operand2;
-    case "/":
-      return operand1 / operand2;
-  }
-}
+//   document.getElementById("result").innerText =
+//     convertToPostfix(expressionArray);
+// }
 
-function backwardCaculator(input) {
-  let stack = [];
-  let temp1 = "";
-  let temp2 = "";
-  input.forEach((element) => {
-    if (!isNaN(element)) {
-      stack.push(parseFloat(element));
-    } else {
-      temp2 = stack.pop();
-      temp1 = stack.pop();
-      stack.push(calculator(temp1, temp2, element));
-    }
-  });
-  let result = stack[0];
-  return result.toString();
-}
+// function convertToPostfix(expressionArray) {
+//   let operators = []; 
+//   let result = [];
 
-document.addEventListener("keydown", (event) => {
-  const buttonKey = event.key;
-  const caculatorlist = ["+", "-", "*", "/"];
+//   expressionArray.reduce((acc, element) => {
+//     if (!isNaN(element)) {
+//       result.push(element);
+//     } else {
+//       while (isOperatorsEmpty(operators) && isHigherPriority(operators[operators.length - 1], element)) {
+//         pushToPostfix(operators, result)
+//       }
+//       operators.push(element);
+//     }
+//   }, null);
 
-  if (!isNaN(buttonKey) || buttonKey === ".") {
-    getUserInput(buttonKey);
-  } else if (caculatorlist.includes(buttonKey)) {
-    getUserInput(buttonKey);
-  } else if (buttonKey === "Enter") {
-    getResult();
-  } else if (buttonKey === "Backspace") {
-    eraseNumber();
-  } else if (buttonKey === "Delete") {
-    allClear();
-  }
-});
+//   while (isOperatorsEmpty(operators)) {
+//     pushToPostfix(operators, result)
+//   }
+
+//   return calculatePostfix(result);
+// }
+
+// function isOperatorsEmpty(operators) {
+//   return operators.length > 0
+// }
+
+// function isHigherPriority(operator1, operator2) {
+//   const priority = { "+": 1, "-": 1, "*": 2, "/": 2 };
+//   return priority[operator1] > priority[operator2];
+// }
+
+// function pushToPostfix(operators, result) {
+//   result.push(operators.pop())
+// }
+
+// function calculatePostfix(input) {
+//   let operandStack = [];
+//   input.forEach((element) => {
+//     if (!isNaN(element)) {
+//       operandStack.push(parseFloat(element));
+//     } else {
+//       const temp2 = operandStack.pop();
+//       const temp1 = operandStack.pop();
+//       operandStack.push(performOperation
+//       (temp1, temp2, element));
+//     }
+//   });
+//   let result = operandStack[0];
+//   return result.toString();
+// }
+
+// function performOperation(operand1, operand2, operator) {
+//   switch (operator) {
+//     case "+":
+//       return add(operand1, operand2)
+//     case "-":
+//       return minus(operand1, operand2)
+//     case "*":
+//       return multiply(operand1, operand2)
+//     case "/":
+//       return devide(operand1, operand2)
+//   }
+// }
+
+// function add(operand1, operand2) {
+//   return operand1 + operand2;
+// }
+
+// function minus(operand1, operand2) {
+//   return operand1 - operand2;
+// }
+
+// function multiply(operand1, operand2) {
+//   return operand1 * operand2;
+// }
+
+// function devide(operand1, operand2) {
+//   if (operand2 === 0) {
+//     return alert("0으로 나눌 수 없습니다!")
+//   }
+//   return operand1 / operand2;
+// }
+
+// document.addEventListener("keydown", (event) => {
+//   const eventKey = event.key;
+
+//   if (!isNaN(eventKey) || eventKey === "." || isOperator(eventKey)) {
+//     getuserExpression(eventKey);
+//   } else if (eventKey === "Enter") {
+//     calculateResult();
+//   } else if (eventKey === "Backspace") {
+//     removeLastCharacter();
+//   } else if (eventKey === "Delete") {
+//     clearAll();
+//   }
+// });
